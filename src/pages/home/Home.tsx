0bdc81ec389851efdbb7bbbfe0f049675d68d6a7
NicolasDevel <nicolassabogal85@gmail.com>
2021-04-17 21:21:14 -0500

    two cards

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import { CardPiewGraphic } from "../../components/card-piew-graphic/CardPiewGrap
 import "./home.scss";
 import store, {Props} from './store';
 import {CardRectangle} from "../../components/card-rectangle/CardRectangle";
+import { CardSmall } from '../../components/card-xs/CardSmall';
 
 class Home extends React.Component<Props> {
   data = [
@@ -68,8 +69,10 @@ class Home extends React.Component<Props> {
             />
           </div>
         </div>
-        <CardRectangle borderColor={'blue'} size={'xs'} colorCircle={'blue'} sizeCircle={'sm'}/>
-        <CardRectangle borderColor={'red'} size={'s'} colorCircle={'red'} sizeCircle={'sm'}/>
+        <CardSmall cardSquare={{borderColor: 'blue', size: 'xs', colorCircle:'blue', sizeCircle:'sm'}} text={'prueba'}/>
+        <CardSmall cardSquare={{borderColor: 'red', size: 's', colorCircle:'red', sizeCircle:'sm'}} text={'prueba'}/>
+
+
         <CardRectangle borderColor={'violet'} size={'l'} backgroundColor={'violet'} colorCircle={'violet'} sizeCircle={'bg'}/>
         <CardRectangle borderColor={'violet'} size={'xl'} colorCircle={'violet'} sizeCircle={'xl'}/>
       </div>
